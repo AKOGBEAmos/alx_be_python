7b16b3f135d416490f53c29d490176b288ee1631
amosakogbe <sp1der@Spydeon.edu>
2024-12-20 14:13:56 +0100

    Updated Basic calculator

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,24 +1,29 @@
 # Interactive calculator
 
-num1 =  int(input("Enter the first number: "))
-num2 =  int(input("Enter the second number: "))
+print("---- The calculator is ON & READY ----")
+choice = 1
+while choice == 1:
+    num1 =  int(input("Enter the first number: "))
+    num2 =  int(input("Enter the second number: "))
 
-#Choose the operation
-operation = input("Choose the operation (+, -, *, /): ")
-match operation:
-    case "+":
-        result = num1 + num2
-        print(f"The result is {result}.")
-    case "-":
-        result = num1 - num2
-        print(f"The result is {result}.")
-    case "*":
-        result = num1* num2
-        print(f"The result is {result}.")
-    case "/":
-        #Handle ZeroDivisionError
-        if num2 == 0:
-            print("Cannot divide by zero.")
-        else:
-            result = num1/num2
+    #Choose the operation
+    operation = input("Choose the operation (+, -, *, /): ")
+    match operation:
+        case "+":
+            result = num1 + num2
             print(f"The result is {result}.")
+        case "-":
+            result = num1 - num2
+            print(f"The result is {result}.")
+        case "*":
+            result = num1* num2
+            print(f"The result is {result}.")
+        case "/":
+            #Handle ZeroDivisionError
+            if num2 == 0:
+                print("Cannot divide by zero.")
+            else:
+                result = num1/num2
+                print(f"The result is {result}.")
+    print("---- The calculator is ON & READY ----")
+    choice = int(input("Do you want to perform another operator (1-Yes | 0 -No) "))
