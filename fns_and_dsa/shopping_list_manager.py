009dcf41f91c1ed6aff335d68c604f3b080a611e
amosakogbe <sp1der@Spydeon.edu>
2025-01-12 19:47:28 +0100

    Updated code for the Shopping_list_manager

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -12,7 +12,7 @@ def main():
         choice = input("Enter your choice: ")
 
         if choice == '1':
-            item = input("Enter the item you want to add to your shopping card: ")
+            item = input("Enter the item to add: ")
             shopping_list.append(item)
             pass
         elif choice == '2':
