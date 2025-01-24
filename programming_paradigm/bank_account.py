9cd4289a8a92ba8e941ed80971b9a49b3a3833a2
h4cker <h4cker@venom.org>
2025-01-24 11:26:20 +0100

    Testing the calculator methods.

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -6,18 +6,22 @@ current_date =  datetime.now()
 current_year = current_date.year
 
 class BankAccount:
+    # A bank account with initial balance of $0
     def __init__(self, balance = 0):
         self.balance = float(balance)
 
     def display_balance(self):
+        """ Account balance display """
         print("Current Balance: ${:.2f}".format(self.balance))
     def deposit(self,amount):
+        """ Deposit operation on the account """
         try:
             self.balance += amount
             return True
         except:
             return False
     def withdraw(self,amount):
+        """ Withdrawal operation on the account """
         if amount <= self.balance:
             self.balance -= amount
             return True
@@ -32,6 +36,7 @@ class BankAccount:
     # Custom function to estimate bank account balance with a certain interest in the future
     
     def account_prediction(self, interest, year):
+        """ Balance prediction on the account applying interest after a certain number of years."""
         duration = year - current_year
         gain = (self.balance * interest /100) * duration
         future_balance = self.balance + gain
---
+++ programming_paradigm/test_simple_calculator.py
@@ -0,0 +1,35 @@
+# Test cases to check operation performed by the calculator
+import unittest
+from simple_calculator import SimpleCalculator
+
+class TestSimpleCalculator(unittest.TestCase):
+    def setUp(self):
+        """Set up the SimpleCalculator instance before each test."""
+        self.calc = SimpleCalculator()
+
+    def test_addition(self):
+        """Test the addition method."""
+        self.assertEqual(self.calc.add(2, 3), 5)
+        self.assertEqual(self.calc.add(-1, 1), 0)
+        self.assertEqual(self.calc.add(3.2, 1), 4.2)
+
+    def test_subtraction(self):
+        """ Test the subtraction method. """
+        self.assertEqual(self.calc.subtract(3,1), 2)
+        self.assertEqual(self.calc.subtract(0,5), -5)
+        self.assertEqual(self.calc.subtract(-3,-6), 3)
+    
+    def test_multiply(self):
+        """Test the multiplication method."""
+        self.assertEqual(self.calc.multiply(2, 3), 6)
+        self.assertEqual(self.calc.multiply(0, 2.0), 0.0)
+        self.assertEqual(self.calc.multiply(3.2, -2.0), -6.4)
+
+    def test_division(self):
+        """Test the division method."""
+        self.assertEqual(self.calc.divide(5, 5), 1.0)
+        self.assertEqual(self.calc.divide(3, 0), None)
+        self.assertEqual(self.calc.divide(6.4, 2), 3.2)
+
+if __name__ == "__main__":
+  unittest.main()
