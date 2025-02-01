6311c9efda01c2d508091e8e994b851cd7355cda
h4cker <h4cker@venom.org>
2025-02-01 15:40:15 +0100

    Applying OOP to a Library system

---
+++ oop/library_system.py
@@ -0,0 +1,40 @@
+# Library system implementation with basic features:
+
+class Book: 
+    def __init__(self, title, author):
+        self.title =  title
+        self.author = author
+
+    def __repr__(self):
+        return f"Book: {self.title} by {self.author}"
+
+
+""" Ebook class inherits from  Book """
+class EBook(Book):
+    def __init__(self, title, author, file_size):
+        super().__init__(title, author)
+        self.size = file_size
+    
+    def __repr__(self):
+        return f"EBook: {self.title} by {self.author}, File Size: {self.size}KB"
+
+""" PrintBook inherits from Book """
+class PrintBook(Book):
+    def __init__(self, title, author, page_count):
+        super().__init__(title, author)
+        self.page = page_count 
+    
+    def __repr__(self):
+        return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page}"
+    
+""" Library is a class that stores all the three categories of books """
+class Library:
+    def __init__(self, books = []):
+        self.books = books
+
+    def add_book(self, book):
+        self.books.append(book)
+    
+    def list_books(self):
+        for book in self.books:
+            print(repr(book))
